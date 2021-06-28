d40732018ff653c1d9bb6de657e37047036eebaa
Katerine Krüger <katerine.kruger@gmail.com>
2021-06-28 17:21:32 -0300

    Aula 05

--- LETMEASK/src/services/firebase.ts
+++ LETMEASK/src/services/firebase.ts
@@ -1,4 +1,4 @@
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
@@ -12,7 +12,7 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APP_ID,
   };
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const database = firebase.database();
